b8c6acd2d2f18fef9b70974475484170cad11e95
Nikita Aksenov <me@nawinds.top>
2024-09-28 09:12:27 +0300

    square docs

--- square.py
+++ square.py
@@ -1,7 +1,20 @@
+def area(a: float) -> float:
+    """Возвращает площадь квадрата со стороной a
 
-def area(a):
+        :param a: длина стороны квадрата
+        :type a: float
+        :returns: площадь квадрата
+        :rtype: float
+    """
     return a * a
 
 
-def perimeter(a):
+def perimeter(a: float) -> float:
+    """Возвращает периметр квадрата со стороной a
+
+        :param a: длина стороны квадрата
+        :type a: float
+        :returns: периметр квадрата
+        :rtype: float
+    """
     return 4 * a
