d5d20b397a4dcf06c4a1db08c68c59179bea2207
Nikita Aksenov <me@nawinds.top>
2024-09-28 09:21:19 +0300

    rectangle docs

--- rectangle.py
+++ rectangle.py
@@ -1,6 +1,24 @@
-def area(a, b): 
+def area(a: float, b: float) -> float:
+    """Возвращает площадь прямоугольника со сторонами a, b
+
+        :param a: длина прямоугольника
+        :type a: float
+        :param b: ширина прямоугольника
+        :type b: float
+        :returns: периметр прямоугольника
+        :rtype: float
+    """
     return a * b 
 
-def perimeter(a, b): 
-    return 2 * (a * b)
+def perimeter(a: float, b: float) -> float:
+    """Возвращает периметр прямоугольника со сторонами a, b
+
+        :param a: длина прямоугольника
+        :type a: float
+        :param b: ширина прямоугольника
+        :type b: float
+        :returns: периметр прямоугольника
+        :rtype: float
+    """
+    return 2 * (a + b)
 
