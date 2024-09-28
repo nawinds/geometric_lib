c067f4fcadc268edf2e97c8c4bfc96fdf246017a
Nikita Aksenov <me@nawinds.top>
2024-09-28 09:38:34 +0300

    circle docs

--- circle.py
+++ circle.py
@@ -1,10 +1,24 @@
 import math
 
 
-def area(r):
+def area(r: float) -> float:
+    """Возвращает площадь круга с радиусом r
+
+        :param r: радиус круга
+        :type r: float
+        :returns: площадь круга
+        :rtype: float
+    """
     return math.pi * r * r
 
 
 def perimeter(r):
+    """Возвращает длину окружности с радиусом r
+
+        :param r: радиус круга
+        :type r: float
+        :returns: длина окружности
+        :rtype: float
+    """
     return 2 * math.pi * r
 
